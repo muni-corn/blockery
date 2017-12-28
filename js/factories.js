9dc4bf5a6a0390ac798ad22f529e755be5d60db1
municorn <municorn@musicaloft.com>
2017-12-28 13:40:54 -0700

    Added Unlockable

--- js/factories.js
+++ js/factories.js
@@ -1,5 +1,4 @@
 /* jshint esversion: 6 */
-/* global VISIBLE_HEIGHT, mouseListeners, Data, Listeners, UI_PADDING, ProgressButton, ImageButton, Board, COLOR_RED, COLOR_ORANGE, COLOR_GREEN, COLOR_BLUE, COLOR_POISON, getStatusBarX,getStatusBarWidth, UI_SANS_TEXT_HEIGHT, getStatusBarHeight, Button, goBackToBoard, CUBE_MESH, toBrowserX, toBrowserY, DIALOG_TITLE_TEXT_HEIGHT, toBrowserH, getSansFont*/
 
 let globalBlockRateMultiplier = 1,
    globalPollutionMultiplier = 1,
@@ -19,7 +18,8 @@ let currentFactoryPage = 0;
 let factoriesMenuYOffset = 0;
 
 class Factory {
-   constructor(index, name, imgSrc, basePrice, blockRate, pollutionRate, capacity) {
+   constructor(factoryCode, index, name, imgSrc, basePrice, blockRate, pollutionRate, capacity) {
+      this.factoryCode = factoryCode;
       this.index = index;
       this.page = index / FACTORIES_PER_PAGE;
       this.name = name;
@@ -254,16 +254,16 @@ class Factory {
 
 // Excuse this mess
 let factories = {
-   smit: new Factory(0, 'Blocksmith', 'img/smit.png', 500, 0.5, 0.1, 50),
-   cott: new Factory(1, 'Cottage Factory', 'img/cott.png', 500 * 15, 0.5 * 4, 0.1 * 5, 50 * 6),
-   mine: new Factory(2, 'Block Mine', 'img/mine.png', 500 * 15 * 15, 0.5 * 4 * 8, 0.1 * 5 * 10, 50 * 6 * 12),
-   powh: new Factory(3, 'Powerhouse', 'img/powh.png', 500 * 15 * 15 * 15, 0.5 * 4 * 8 * 12, 0.1 * 5 * 10 * 15, 50 * 6 * 12 * 18),
-   clmk: new Factory(4, 'Cloudmaker', 'img/clmk.png', 500 * 15 * 15 * 15 * 15, 0.5 * 4 * 8 * 12 * 16, 0.1 * 5 * 10 * 15 * 20, 50 * 6 * 12 * 18 * 24),
-   volc: new Factory(5, 'Block Volcano', 'img/volc.png', 500 * 15 * 15 * 15 * 15 * 15, 0.5 * 4 * 8 * 12 * 16 * 20, 0.1 * 5 * 10 * 15 * 20 * 25, 50 * 6 * 12 * 18 * 24 * 30),
-   mnfm: new Factory(6, 'Moon Block Farm', 'img/mnfm.png', 500 * 15 * 15 * 15 * 15 * 15 * 15, 0.5 * 4 * 8 * 12 * 16 * 20 * 24, 0.1 * 5 * 10 * 15 * 20 * 25 * 30, 50 * 6 * 12 * 18 * 24 * 30 * 36),
-   plsm: new Factory(7, 'Planetary Block Storm', 'img/plsm.png', 500 * 15 * 15 * 15 * 15 * 15 * 15 * 15, 0.5 * 4 * 8 * 12 * 16 * 20 * 24 * 28, 0.1 * 5 * 10 * 15 * 20 * 25 * 30 * 35, 50 * 6 * 12 * 18 * 24 * 30 * 36 * 42),
-   star: new Factory(8, 'Star Reactor', 'img/star.png', 500 * 15 * 15 * 15 * 15 * 15 * 15 * 15 * 15, 0.5 * 4 * 8 * 12 * 16 * 20 * 24 * 28 * 32, 0.1 * 5 * 10 * 15 * 20 * 25 * 30 * 35 * 40, 50 * 6 * 12 * 18 * 24 * 30 * 36 * 42 * 48),
-   dmgt: new Factory(9, 'Interdimensional Gateway', 'img/dmgt.png', 500 * 15 * 15 * 15 * 15 * 15 * 15 * 15 * 15 * 15, 0.5 * 4 * 8 * 12 * 16 * 20 * 24 * 28 * 32 * 36, 0.1 * 5 * 10 * 15 * 20 * 25 * 30 * 35 * 40 * 45, 50 * 6 * 12 * 18 * 24 * 30 * 36 * 42 * 48 * 54)
+   smit: new Factory("smit", 0, 'Blocksmith', 'img/smit.png', 500, 0.5, 0.1, 50),
+   cott: new Factory("cott", 1, 'Cottage Factory', 'img/cott.png', 500 * 15, 0.5 * 4, 0.1 * 5, 50 * 6),
+   mine: new Factory("mine", 2, 'Block Mine', 'img/mine.png', 500 * 15 * 15, 0.5 * 4 * 8, 0.1 * 5 * 10, 50 * 6 * 12),
+   powh: new Factory("powh", 3, 'Powerhouse', 'img/powh.png', 500 * 15 * 15 * 15, 0.5 * 4 * 8 * 12, 0.1 * 5 * 10 * 15, 50 * 6 * 12 * 18),
+   clmk: new Factory("clmk", 4, 'Cloudmaker', 'img/clmk.png', 500 * 15 * 15 * 15 * 15, 0.5 * 4 * 8 * 12 * 16, 0.1 * 5 * 10 * 15 * 20, 50 * 6 * 12 * 18 * 24),
+   volc: new Factory("volc", 5, 'Block Volcano', 'img/volc.png', 500 * 15 * 15 * 15 * 15 * 15, 0.5 * 4 * 8 * 12 * 16 * 20, 0.1 * 5 * 10 * 15 * 20 * 25, 50 * 6 * 12 * 18 * 24 * 30),
+   mnfm: new Factory("mnfm", 6, 'Moon Block Farm', 'img/mnfm.png', 500 * 15 * 15 * 15 * 15 * 15 * 15, 0.5 * 4 * 8 * 12 * 16 * 20 * 24, 0.1 * 5 * 10 * 15 * 20 * 25 * 30, 50 * 6 * 12 * 18 * 24 * 30 * 36),
+   plsm: new Factory("plsm", 7, 'Planetary Block Storm', 'img/plsm.png', 500 * 15 * 15 * 15 * 15 * 15 * 15 * 15, 0.5 * 4 * 8 * 12 * 16 * 20 * 24 * 28, 0.1 * 5 * 10 * 15 * 20 * 25 * 30 * 35, 50 * 6 * 12 * 18 * 24 * 30 * 36 * 42),
+   star: new Factory("star", 8, 'Star Reactor', 'img/star.png', 500 * 15 * 15 * 15 * 15 * 15 * 15 * 15 * 15, 0.5 * 4 * 8 * 12 * 16 * 20 * 24 * 28 * 32, 0.1 * 5 * 10 * 15 * 20 * 25 * 30 * 35 * 40, 50 * 6 * 12 * 18 * 24 * 30 * 36 * 42 * 48),
+   dmgt: new Factory("dmgt", 9, 'Interdimensional Gateway', 'img/dmgt.png', 500 * 15 * 15 * 15 * 15 * 15 * 15 * 15 * 15 * 15, 0.5 * 4 * 8 * 12 * 16 * 20 * 24 * 28 * 32 * 36, 0.1 * 5 * 10 * 15 * 20 * 25 * 30 * 35 * 40 * 45, 50 * 6 * 12 * 18 * 24 * 30 * 36 * 42 * 48 * 54)
    // The Everything Dimension?
 };
 
@@ -311,7 +311,7 @@ const renderFactoryMenuScoreboard = (gl, programInfo, ctx2d) => {
    let y = backToBoardButton.y - h;
    CUBE_MESH.render(gl, x, y, 0, w, h, Board.BLOCK_WIDTH);
 
-   // Set the text color 
+   // Set the text color
    ctx2d.fillStyle = 'white';
 
    // Get fonts
