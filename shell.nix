ece13dc13b00ef268ff2012cf008e88ce7dceba2
municorn <municorn@musicaloft.com>
2021-08-22 20:46:28 -0600

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+mkShell {
+  buildInputs = [
+    cargo
+    pkgconfig
+    udev
+    alsaLib
+    lutris
+    x11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    vulkan-tools
+    vulkan-headers
+    vulkan-loader
+    vulkan-validation-layers
+  ];
+}
