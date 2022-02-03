5e3bd2d7c3af47dad7c675446567c36d32dee345
municorn <municorn@musicaloft.com>
2022-02-02 20:59:22 -0600

    Use Rust nightly

--- flake.nix
+++ flake.nix
@@ -1,14 +1,19 @@
 {
   description = "Blockery, the tile-matching clicker game";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem
       (system:
       let
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system;
+          inherit system overlays;
           config = { allowUnfree = true; };
         };
       in 
